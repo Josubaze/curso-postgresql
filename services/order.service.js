e488fe3a83fba8cfa35d646fed671ed983fadfb6
Josue Baez <josuejbaeze4@gmail.com>
2024-02-07 18:27:21 -0400

    Órdenes de compra, relacion de 1 a *

--- services/order.service.js
+++ services/order.service.js
@@ -1,29 +1,42 @@
 const boom = require('@hapi/boom');
+const { models } = require('./../libs/sequelize');
 
 class OrderService {
 
   constructor(){
   }
   async create(data) {
-    return data;
+    const newOrder = await models.Order.create(data);
+    return newOrder;
   }
 
   async find() {
-    return [];
+    const rta = await models.Order.findAll();
+    return rta ;
   }
 
   async findOne(id) {
-    return { id };
+    const order = await models.Order.findByPk(id, {
+      include: [{
+        association: 'customer',
+        include: ['user'],
+      }],
+    });
+    if(!order){
+      throw boom.notFound('Order not found');
+    }
+    return order;
   }
 
   async update(id, changes) {
-    return {
-      id,
-      changes,
-    };
+    const order = await this.findOne(id);
+    const rta = await order.update(changes);
+    return rta;
   }
 
   async delete(id) {
+    const order = await this.findOne(id);
+    await order.destroy()
     return { id };
   }
 
